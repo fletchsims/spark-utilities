f28a0c1c87629bd98294b48387ee28550539ecce
fletchersims <fletcher.sims@gmail.com>
2024-01-26 09:23:08 -0500

    Update build file, fix format file, and add in some basic timestamp functions and haversine formula

--- build.sbt
+++ build.sbt
@@ -2,7 +2,17 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.2"
 
+val SparkVersion = "3.3.2"
+
+val sparkDependencies = Seq(
+  "org.apache.spark" %% "spark-core" % SparkVersion,
+  "org.apache.spark" %% "spark-sql"  % SparkVersion
+)
+
 lazy val root = (project in file("."))
   .settings(
-    name := "spark-utilities"
+    name := "spark-utilities",
+    libraryDependencies ++= sparkDependencies.map(_ % "provided"),
+    assemblyPackageScala / assembleArtifact := false,
+    assemblyPackageDependency / assembleArtifact := false
   )
--- src/main/scala/com/arrow/sql/functions.scala
+++ src/main/scala/com/arrow/sql/functions.scala
@@ -1,3 +1,36 @@
 package com.arrow.sql
 
-object functions {}
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.TimestampType
+
+object functions {
+
+  private val MillisInSecond = 1000L
+  private val EarthRadiusMeters: Column = lit(6378137.0)
+
+  def millisToTimestamp(millisCol: Column): Column = {
+    (millisCol / MillisInSecond).cast(TimestampType).alias(millisCol.toString())
+  }
+
+  def millisToLocalTimestamp(millisCol: Column, timezone: Column): Column = {
+    from_utc_timestamp(millisToTimestamp(millisCol), timezone).alias(millisCol.toString())
+  }
+
+  def distanceInMeters(x1: Column, y1: Column, x2: Column, y2: Column): Column = {
+    val x1Rad = radians(x1)
+    val y1Rad = radians(y1)
+    val x2Rad = radians(x2)
+    val y2Rad = radians(y2)
+
+    val t = asin(
+      sqrt(
+        pow(sin((x1Rad - x2Rad) / 2), 2) + cos(x1Rad) * cos(x2Rad) * pow(
+          sin((y1Rad - y2Rad) / 2),
+          2
+        )
+      )
+    )
+    lit(2) * EarthRadiusMeters * t
+  }
+}
