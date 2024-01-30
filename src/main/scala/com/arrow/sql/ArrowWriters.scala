5cbe19a5b75cd0104a7ad5447ccd76c5bd1d48b0
fletchersims <fletcher.sims@gmail.com>
2024-01-30 09:52:16 -0500

    update the writers

--- src/main/scala/com/arrow/sql/ArrowWriters.scala
+++ src/main/scala/com/arrow/sql/ArrowWriters.scala
@@ -10,22 +10,29 @@ object ArrowWriters extends SparkService {
   def createEmptyDataset[T: Encoder](): Dataset[T] = {
     Seq.empty[T].toDS
   }
-  def writeParquet(ds: Dataset[_], path: String, saveMode: SaveMode = SaveMode.Append): Unit =
-    ds.write.mode(saveMode).parquet(path)
+
+  def writeFile(
+      ds: Dataset[_],
+      path: String,
+      saveMode: SaveMode = SaveMode.Append,
+      format: String = "parquet"
+  ): Unit =
+    ds.write.mode(saveMode).format(format).save(path)
+
   def checkDatasetAndWrite[T: Encoder](
-      dataset: Try[Dataset[T]],
+      ds: Try[Dataset[T]],
       outputPath: String,
       saveMode: SaveMode = SaveMode.Append
   ): Unit = {
-    dataset match {
+    ds match {
       case Failure(e) =>
         val emptyDataset = createEmptyDataset[T]()
         println(s"Failure during the process. Writing empty dataset. Error message: $e")
-        writeParquet(emptyDataset, outputPath, saveMode)
+        writeFile(emptyDataset, outputPath, saveMode)
 
       case Success(_) =>
         println("Processed successfully. Writing dataset...")
-        dataset.map(finalDataset => writeParquet(finalDataset, outputPath, saveMode))
+        ds.map(finalDataset => writeFile(finalDataset, outputPath, saveMode))
     }
     println("Dataset is written successfully")
   }
