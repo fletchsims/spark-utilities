ca82113d228f2fba023ac1735adb6c52e30c7c0e
fletchersims <fletcher.sims@gmail.com>
2024-01-30 10:25:02 -0500

    add in some documentation for the getColumns function

--- src/main/scala/com/arrow/sql/types/StructTypeHelpers.scala
+++ src/main/scala/com/arrow/sql/types/StructTypeHelpers.scala
@@ -5,6 +5,14 @@ import org.apache.spark.sql.types.{DataType, StructType}
 import scala.collection.mutable.ListBuffer
 
 object StructTypeHelpers {
+  /**
+   * Returns a list of column names present in a given data type.
+   *
+   * @param dt            the data type to extract column names from
+   * @param path          a prefix path for nested fields
+   * @param listOfColumns a mutable list buffer to store the column names
+   * @return a list of column names extracted from the input data type
+   */
   def getColumns(
       dt: DataType,
       path: String = "",
